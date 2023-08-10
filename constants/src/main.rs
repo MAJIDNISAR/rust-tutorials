aab965df4e2373e8cea1a5d6530cfe879bec659d
Majid Nisar <majidnisar@iQuasars-MacBook-Pro-8.local>
2023-08-10 19:38:13 +0530

    comment

--- constants/src/main.rs
+++ constants/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    // type and value of a constant cant change in the program
+    // type and value of a constant cant change in the program and cannot redefine the constants
     const SECONDS_IN_MINUTES: u32= 60;
     println!("{} seconds in a minute",SECONDS_IN_MINUTES);
 }
